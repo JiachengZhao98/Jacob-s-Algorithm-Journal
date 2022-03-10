4f19e417b7a276d2e455e2649b572bfeebf4351b
JiachengZhao98 <78194255+JiachengZhao98@users.noreply.github.com>
2022-03-09 22:24:38 -0600

    change

--- leetcode_56/leetcode_56/main.cpp
+++ leetcode_56/leetcode_56/main.cpp
@@ -6,41 +6,28 @@
 #include "algorithm"
 using namespace std;
 
-//class Solution {
-//public:
-//    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//        sort(intervals.begin(), intervals.end());
-//        vector<vector<int>>::iterator iter = intervals.begin();
-//        for (int i = 0; i < intervals.size(); i++) {
-//            if ((i + 1) < intervals.size() && (intervals[i][1] >= intervals[i + 1][0])) {
-//                intervals[i + 1][0] = min(intervals[i][0], intervals[i + 1][0]);
-//                intervals[i + 1][1] = max(intervals[i + 1][1], intervals[i][1]);
-//                intervals.erase(iter + i);
-//                i--;
-//                iter = intervals.begin();
-//            }
-//        }
-//        return intervals;
-//    }
-//};  exceeds time limit in the last case. SHIT.
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& nums) {
-        int n = nums.size();
-        if (n < 2) return nums;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        for (int i = 0, left, right; i < n;) {
-            left = nums[i][0];
-            right = nums[i ++][1];
-            while (i < n && nums[i][0] <= right)
-                right = max(right, nums[i ++][1]);
-            res.push_back({left, right});
+        for (int i = 0; i < intervals.size(); i++) {
+            if ((i + 1) < intervals.size() && (intervals[i][1] >= intervals[i + 1][0])) {
+                intervals[i + 1][0] = min(intervals[i][0], intervals[i + 1][0]);
+                intervals[i + 1][1] = max(intervals[i + 1][1], intervals[i][1]);
+            }
+            else {
+                res.push_back({intervals[i][0],intervals[i][1]});
+            }
         }
         return res;
     }
 };
+
+
 int main() {
+    
+    // test for vector.erase() function
     vector<vector<int>> test = {{1,2}, {3,4}};
     vector<vector<int>>::iterator iter;
     iter = test.begin();
