c82636349c20779274d3cb6cf005342fe67c6b1d
JiachengZhao98 <78194255+JiachengZhao98@users.noreply.github.com>
2022-03-22 13:51:15 -0500

    lc_150

---
+++ 150_ Reverse Polish Notation/150_ Reverse Polish Notation/main.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  150_ Reverse Polish Notation
+//
+//  Created by Jiacheng Zhao on 3/22/22.
+//
+
+#include <iostream>
+#include "stack"
+#include "vector"
+#include "string"
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> mySta;
+        int res = -1;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int temp1 = mySta.top();
+                mySta.pop();
+                int temp2 = mySta.top();
+                mySta.pop();
+                char *operator1 = (char *)tokens[i].c_str();
+                switch (int(*operator1)) {
+                    case 42:
+                        res = temp1 * temp2;
+                        mySta.push(res);
+                        break;
+                    case 43:
+                        res = temp1 + temp2;
+                        mySta.push(res);
+                        break;
+                    case 45:
+                        res = temp2 - temp1;
+                        mySta.push(res);
+                        break;
+                    case 47:
+                        res = temp2 / temp1;
+                        mySta.push(res);
+                        break;
+                }
+            }
+            else mySta.push(stoi(tokens[i]));
+        }
+        return mySta.top();
+    }
+};
+
+int main() {
+    // insert code here...
+    
+    return 0;
+}
