55aa252bbbac84f2ae83572e1e36a7fd213f36a2
JiachengZhao98 <78194255+JiachengZhao98@users.noreply.github.com>
2022-07-03 01:33:38 -0500

    chg

--- binary_trees/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ binary_trees/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -15,17 +15,17 @@ using namespace std;
  
 class Solution {
 public:
-    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder) {
-        if (postorder.size() == 0) {
+    TreeNode* traversal(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0) {
             return nullptr;
         }
-        int rootValue = postorder[postorder.size() - 1];  // the last value in postorder vector is root
+        int rootValue = preorder[0];                      // the first value in preorder vector is root
         TreeNode* root = new TreeNode(rootValue);
-        if (postorder.size() == 1) {                      
+        if (preorder.size() == 1) {                      
             return root;                                  // if there is only one value in postorder vector
         }
         int delimiterIndex;
-        for (delimiterIndex = 0; delimiterIndex < postorder.size(); delimiterIndex++) {
+        for (delimiterIndex = 0; delimiterIndex < preorder.size(); delimiterIndex++) {
             if (inorder[delimiterIndex] == rootValue) {
                 break;
             }
@@ -33,18 +33,20 @@ public:
         vector<int> leftInorder(inorder.begin(), inorder.begin() + delimiterIndex);
         vector<int> rightInorder(inorder.begin() + 1 + delimiterIndex, inorder.end());
 
-        postorder.resize(postorder.size() - 1);
+        reverse(preorder.begin(), preorder.end());
+        preorder.resize(preorder.size() - 1);
+        reverse(preorder.begin(), preorder.end());
 
-        vector<int> leftPostorder(postorder.begin(), postorder.begin() + leftInorder.size());
-        vector<int> rightPostorder(postorder.begin() + leftInorder.size(), postorder.end());
+        vector<int> leftPreorder(preorder.begin(), preorder.begin() + leftInorder.size());
+        vector<int> rightPreorder(preorder.begin() + leftInorder.size(), preorder.end());
 
-        root->left = traversal(leftInorder, leftPostorder);
-        root->right = traversal(rightInorder, rightPostorder);
+        root->left = traversal(leftPreorder, leftInorder);
+        root->right = traversal(rightPreorder, rightInorder);
     
         return root;
     }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        TreeNode* root = traversal(inorder, postorder);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* root = traversal(preorder, inorder);
         return root;
     }
 };
